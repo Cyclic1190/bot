6ac92d6b43fc0190bccd662de544046850a47bde
Cyclic1190 <61198979+Cyclic1190@users.noreply.github.com>
2020-04-12 01:03:47 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ bot.on('message', message =>{
     var msg = message.content;
     var prefix = ">" //The text before commands, you can put anything that you prefer
 
-    if(message.author.id != "381443488392413184" && message.channel.id === "381397780385628181"){
+    if(message.author.id != "381443488392413184" && message.channel.id === "688776117011742780"){
         if(msg.startsWith('>', 0)){
-            if(msg === prefix + "bang" && message.channel.id === "381397780385628181"){
+            if(msg === prefix + "bang" && message.channel.id === "688776117011742780"){
                 message.channel.send('BOOM!!!!!') // Sends a message to the channel, with the content
             }
             else{
